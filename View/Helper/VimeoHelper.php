f09db393178b5cbeae39c3b58dec9902ad79aebe
ondra <ondra@on-idle.com>
2012-06-25 10:43:08 +0100

    VimeoHelper added

---
+++ View/Helper/VimeoHelper.php
@@ -0,0 +1,44 @@
+<?php
+class VimeoHelper extends AppHelper {
+
+	public $helpers = array('Html', 'Form');
+
+	/* $options:
+	*	album - set album to show
+	*	url - url fot connect link back addres
+	*	optionOnly - if true, only options returned
+	*/
+	public function input($options = array()) {
+		extract($options);
+		if (empty($album)) {
+			$album = AppPreference::get('vimeo_album');
+		}
+		$videos = ClassRegistry::init('Vimeo.VimeoAlbums')->getVideosListByTitle($album);
+
+		$connectUrl = array(
+			'plugin' => 'vimeo', 'controller' => 'vimeo',
+			'action' => 'connect'
+		);
+		if (isset($url)) {
+			$url = serialize($url);
+			$url = bin2hex($url);
+			$connectUrl[] = $url;
+		}
+
+		$options = array(
+			'type' => 'select',
+			'options' => isset($videos) ? $videos : null,
+			'between' => 
+				$this->Html->div('note', "please ".$this->Html->link('connect', $connectUrl)
+				." CMS with your Vimeo account to see list with your videos"),
+		);
+		if (isset($optionsOnly) && $optionsOnly) {
+			return $options;
+		} else {
+			return $this->Form->inputs(array(
+				'legend' => __('Video (*optional)'),
+				'video_id' => $options 
+			));
+		}
+	}
+}
